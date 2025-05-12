edd3c999ade04e799c174976c95288d9b95a528d
Amy-Grace <ymanduta@gmail.com>
2025-05-12 07:52:10 +0300

    first commit

---
+++ app/src/main/java/com/example/parkingactivity/data/models/Ticket.kt
@@ -0,0 +1,10 @@
+package com.example.parkingactivity.data.models
+
+data class Ticket(
+    val ticketId: String = "",
+    val spotId: String = "",
+    val userId: String = "",
+    val startTime: Long = 0L,
+    val endTime: Long = 0L,
+    val amountPaid: Double = 0.0
+)
---
+++ app/src/main/java/com/example/parkingactivity/data/models/User.kt
@@ -0,0 +1,9 @@
+package com.example.parkingactivity.data.models
+
+data class User(
+val userId: String = "",
+val name: String = "",
+val email: String = "",
+val password: String = ""
+)
+
